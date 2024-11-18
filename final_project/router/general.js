a79064577f409c0abe6cc3ca9d7b41d08f2e9aef
Luigi Venturi <luigi.venturi.ie@gmail.com>
2024-11-18 08:08:38 -0500

    task 4,5,6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,15 +8,22 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    console.log(users)
+    const username = req.body.username;
+    const password = req.body.password;
+    if(users.find(user=>user.username === username)){
+        return res.status(300).json({message: `User ${username} already exists.`});
+    }else{
+        users.push({username:username, password:password})
+        res.status(200).json({message: `User ${username} was registered.`})
+    }
 });
 
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   res.send(books)
-//   res.send(JSON.stringify(books))
+  res.send(books)
 });
 
 
@@ -42,15 +49,23 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    console.log(title)
+    const filtered = []
+    Object.keys(books).forEach(key=>{
+        if(books[key].title === title){
+            filtered.push(books[key])
+        }
+    })
+
+    res.send(filtered);
 });
 
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 
